3142f9c517feb7fdc54a55b37276e0de76ac3dc3
Tiago Zini <tiagozini@gmail.com>
2022-11-06 23:20:25 -0300

    Atualizando acessos do firestore

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: "localhost:4000",
+  firebaseConfig : {
+    apiKey: "AIzaSyBce6jOte3DLLwtL0hLrYr6_csLzFh1q78",
+    authDomain: "bet033.firebaseapp.com",
+    projectId: "bet033",
+    storageBucket: "bet033.appspot.com",
+    messagingSenderId: "485819557713",
+    appId: "1:485819557713:web:22eaafb1202f970e275570"
+  }
 };
